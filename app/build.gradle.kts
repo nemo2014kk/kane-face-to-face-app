4216b4b4174d5f455b132fe22c02b94c1e374a6f
nemo2014kk <nemo2014kk@gmail.com>
2026-06-07 08:49:05 +0200

    v3.0.1 | 版本号更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.firstapp"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.6"
+        versionCode = 3
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
